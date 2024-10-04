26c7eaea56c3a7f6ed5f3dd0914718942066a0cf
Piyanshu-Zade <zpiyanshu@gmail.com>
2024-10-04 23:56:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('.menu')
+const toggle = document.querySelector('#toggle')
+const addListing = document.getElementById('add-listing')
+
+addListing.addEventListener('click', () => {
+  alert("Coming soon....")
+})
+
+toggle.addEventListener("click", function(){
+    menu.classList.toggle('show_menu')
+})
+
+var swiper = new Swiper(".about_swiper", {
+    loop:true,
+    // autoplay:true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: ".progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
+
